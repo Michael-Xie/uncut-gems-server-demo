9dcb8cd830e662002539a81b63c8c95e4d1bb4ed
Anthony Pisani <antoineincognito@hotmail.com>
2020-02-13 16:00:53 -0500

    .

--- src/routes/users.js
+++ src/routes/users.js
@@ -6,9 +6,24 @@ module.exports = db => {
       `
       SELECT * FROM users
       `
-    ).then(({ rows: users }) => {
+    )
+    .then(({ rows: users }) => {
       response.json(users)
     })
   })
+
+  router.post("/users", (request, response) => {
+    const {user_name, password, wallet_amount, stripe_charge_id} = request.body
+    db.query(
+      `
+      INSERT INTO users (
+        user_name, password, wallet_amount, stripe_charge_id
+      ) VALUES (
+        $1::text, $2::text, $3::integer, $4::integer 
+      )
+      `, [user_name, password, wallet_amount, stripe_charge_id]
+    )
+  })
+
   return router
 }

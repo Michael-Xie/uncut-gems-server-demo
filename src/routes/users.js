9b9a6c3688a757f5715405c9ddabcfcc6fd04f7f
Michael-Xie <xiemicha@gmail.com>
2020-02-14 18:45:05 -0500

    Adjust register user to return newly created user object

--- src/routes/users.js
+++ src/routes/users.js
@@ -47,15 +47,29 @@ module.exports = db => {
 
   router.post("/users", (request, response) => {
     const { user_name, password, wallet_amount, stripe_charge_id } = request.body
-    db.query(
-      `
-      INSERT INTO users (
-        user_name, password, wallet_amount, stripe_charge_id
-      ) VALUES (
-        $1::text, $2::text, $3::integer, $4::integer 
-      )
-      `, [user_name, password, wallet_amount, stripe_charge_id]
-    )
+    getUserWithUsername(db, user_name)
+    .then((result) => {
+      const user = result.rows[0];
+      if (!user && password.length > 0) {
+        console.log("inside users route")
+        db.query(
+          `
+          INSERT INTO users (
+            user_name, password, wallet_amount, stripe_charge_id
+          ) VALUES (
+            $1::text, $2::text, $3::integer, $4::integer 
+          ) RETURNING *
+          `, [user_name, password, wallet_amount, stripe_charge_id]
+        ).then((res) => {
+          console.log("newly inserted", res.rows[0]);
+          response.json(res.rows[0]);
+        })
+      } else {
+        console.log("no user found / password empty");
+        response.json({})
+      }
+    })
+    .catch((err) => response.send(err));
   })
 
   return router

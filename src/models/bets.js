4865e475c037abb9eb4fd9aaaf184f3328c0ff5e
Michael-Xie <xiemicha@gmail.com>
2020-02-18 20:38:35 -0500

    Add helper functions to query needed informations to evaluate bets made by players

--- src/models/bets.js
+++ src/models/bets.js
@@ -2,16 +2,43 @@ module.exports = {
   getUserBetsByParlay: function(db, parlayId) {
     const queryString =
       `
-      SELECT * FROM bets
-        JOIN user_bets ON bet_id = bets.id
-
+      SELECT * FROM user_bets
       WHERE user_bets.parlay_id = $1;
       `;
     const values = [parlayId]
     return db.query(queryString, values)
-      // .then((res) => res.rows); 
+  },
+  getBetsByParlay: function(db, parlayId) {
+    const queryString =
+    `
+    SELECT * FROM bets
+    WHERE parlay_id = $1;
+    `
+    const values = [parlayId]
+    return db.query(queryString, values);
+  },
+  getGamesByParlay: function(db, parlayId) {
+    const queryString = 
+    `
+    SELECT game_id FROM parlays
+    JOIN bets ON parlay_id = parlays.id
+    WHERE parlay_id = $1;
+    `
+    const values = [parlayId];
+    return db.query(queryString, values);
+  },
+  getGameScoresByGames: function(db, games) {
+    let queryString =
+    `
+    SELECT * FROM games
+    JOIN game_scores ON games.game_id=game_scores.game_id
+    WHERE
+    `
+    let condition = games.map((game, index)=>{
+       return `games.game_id=$${index+1}`
+    }).join(' OR ');
+    queryString += condition;
+    const values = [...games]
+    return db.query(queryString, values);
   }
-  // getGameInfoByGames: function(db, games) {
-
-  // }
 }

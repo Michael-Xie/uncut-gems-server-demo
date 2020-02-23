9c341f69ca017e1054f0ef6a1904d370fc616daf
Michael-Xie <xiemicha@gmail.com>
2020-02-23 12:37:45 -0500

    Add more instructions for heroku

--- src/application.js
+++ src/application.js
@@ -47,6 +47,8 @@ Switching between mock and rapidapi servers:
 - Go to getGames and getScores and set useMock to false to use rapidapi and vice versa
 
 Heroku:
+- `npm install -g heroku`
+
 - Changes can be pushed by:
 `git push heroku master`
 

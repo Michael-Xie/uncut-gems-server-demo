fb7a1170e29675d7182f7dd307bf1e80958a635e
Michael-Xie <xiemicha@gmail.com>
2020-02-25 13:22:30 -0500

    Adjust time for score and game update 30, 60s

--- src/application.js
+++ src/application.js
@@ -90,11 +90,11 @@ module.exports = function application(ENV, actions = { updateState: () => {}}) {
     getScores(date, db)
     axios.get(`/api/global/1`, {baseURL: 'https://uncut-gems-api-server.herokuapp.com'})
       .catch(err => console.log(err))
-  }, 120000)
+  }, 30000)
 
   setInterval(() => {
     getGames(date, db, true)
-  }, 240000)
+  }, 60000)
   
   app.use("/api/pay", payRoute(db, moneyHelper));
   app.use("/api/test", testRoute(db, betsHelper))
